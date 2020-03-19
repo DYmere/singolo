3377fba5c74340a1302ea30e3c9b2b52b1cb3be8
Dzmitry Yatsyna <dyfromby@gmail.com>
2020-03-19 18:34:35 +0300

    fix: information window in section 'Get a quote' and add switching menu

--- script.js
+++ script.js
@@ -7,22 +7,51 @@ menu.addEventListener("click", (event) => {
   event.target.classList.add("active");
 });
 
-button.addEventListener("click", () => {
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset < 300) {
+    menu.querySelectorAll("a").forEach(element => element.classList.remove("active"));
+    menu.querySelector("li:nth-child(1) a").classList.add("active");
+  }
+  else if (window.pageYOffset >= 300 && window.pageYOffset < 700) {
+    menu.querySelectorAll("a").forEach(element => element.classList.remove("active"));
+    menu.querySelector("li:nth-child(2) a").classList.add("active");
+  }
+  else if (window.pageYOffset >= 700 && window.pageYOffset < 1500) {
+    menu.querySelectorAll("a").forEach(element => element.classList.remove("active"));
+    menu.querySelector("li:nth-child(3) a").classList.add("active");
+  }
+  else if (window.pageYOffset >= 1500 && window.pageYOffset < 2000) {
+    menu.querySelectorAll("a").forEach(element => element.classList.remove("active"));
+    menu.querySelector("li:nth-child(4) a").classList.add("active");
+  }
+  else if (window.pageYOffset >= 2000) {
+    menu.querySelectorAll("a").forEach(element => element.classList.remove("active"));
+    menu.querySelector("li:nth-child(5) a").classList.add("active");
+  };
+});
+
+button.addEventListener("click", (event) => {
+  
   const input_subject = document.getElementById("input-subject").value.toString();
   const input_description = document.getElementById("input-description").value.toString();
-  if (input_subject) {
-    document.getElementById("subject").innerText = "Subject: "+input_subject;
-  }
-  else {
-    document.getElementById("subject").innerText = "without subject";
+  const input_name = document.getElementById("input-name").value.toString();
+  const input_email = document.getElementById("input-email").value.toString();
+  if (input_name && input_email) {
+    if (input_subject) {
+      document.getElementById("subject").innerText = "Subject: "+input_subject;
+    }
+    else {
+      document.getElementById("subject").innerText = "without subject";
+    };
+    if (input_description) {
+      document.getElementById("description").innerText = "Description: "+input_description;
+    }
+    else {
+      document.getElementById("description").innerText = "without description";
+    };
+    document.getElementById("message-block").classList.remove("hidden");
+    event.preventDefault();
   };
-  if (input_description) {
-    document.getElementById("description").innerText = "Description: "+input_description;
-  }
-  else {
-    document.getElementById("description").innerText = "without description";
-  };
-  document.getElementById("message-block").classList.remove("hidden");
 });
 
 close_button.addEventListener("click", () => {
