f2d373e4247de9084ffa69ae9c58d8f515af2ee2
Dzmitry Yatsyna <dyfromby@gmail.com>
2020-03-20 19:57:44 +0300

    feat: add activating phone screens

--- script.js
+++ script.js
@@ -3,11 +3,9 @@ const portfolio_menu = document.querySelector(".portfolio nav");
 const button = document.getElementById("submit");
 const close_button = document.getElementById("close-button");
 const gallery = document.querySelector(".gallery");
+const ibutton_ver = document.getElementById("ibutton-ver");
+const ibutton_hor = document.getElementById("ibutton-hor");
 
-menu.addEventListener("click", (event) => {
-  menu.querySelectorAll("a").forEach(element => element.classList.remove("active"));
-  event.target.classList.add("active");
-});
 
 gallery.addEventListener("click", (event) => {
   gallery.querySelectorAll("img").forEach(element => element.classList.remove("active-img"));
@@ -26,29 +24,6 @@ portfolio_menu.addEventListener("click", (event) => {
   document.querySelector(".gallery").innerText = "";
   mixed_arr_gallery.forEach(element => document.querySelector(".gallery").appendChild(element));
   event.preventDefault();
-});
-
-window.addEventListener('scroll', () => {
-  if (window.pageYOffset < 695) {
-    menu.querySelectorAll("a").forEach(element => element.classList.remove("active"));
-    menu.querySelector("li:nth-child(1) a").classList.add("active");
-  }
-  else if (window.pageYOffset >= 695 && window.pageYOffset < 1195) {
-    menu.querySelectorAll("a").forEach(element => element.classList.remove("active"));
-    menu.querySelector("li:nth-child(2) a").classList.add("active");
-  }
-  else if (window.pageYOffset >= 1195 && window.pageYOffset < 2064) {
-    menu.querySelectorAll("a").forEach(element => element.classList.remove("active"));
-    menu.querySelector("li:nth-child(3) a").classList.add("active");
-  }
-  else if (window.pageYOffset >= 2064 && window.pageYOffset < 2798) {
-    menu.querySelectorAll("a").forEach(element => element.classList.remove("active"));
-    menu.querySelector("li:nth-child(4) a").classList.add("active");
-  }
-  else if (window.pageYOffset >= 2798) {
-    menu.querySelectorAll("a").forEach(element => element.classList.remove("active"));
-    menu.querySelector("li:nth-child(5) a").classList.add("active");
-  };
 });
 
 button.addEventListener("click", (event) => {
@@ -79,4 +54,52 @@ close_button.addEventListener("click", () => {
   document.getElementById("subject").innerText = "";
   document.getElementById("description").innerText = "";
   document.getElementById("message-block").classList.add("hidden");
+});
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset < 545) {
+    menu.querySelectorAll("a").forEach(element => element.classList.remove("active"));
+    menu.querySelector("li:nth-child(1) a").classList.add("active");
+  }
+  else if (window.pageYOffset >= 545 && window.pageYOffset < 1050) {
+    menu.querySelectorAll("a").forEach(element => element.classList.remove("active"));
+    menu.querySelector("li:nth-child(2) a").classList.add("active");
+  }
+  else if (window.pageYOffset >= 1050 && window.pageYOffset < 1920) {
+    menu.querySelectorAll("a").forEach(element => element.classList.remove("active"));
+    menu.querySelector("li:nth-child(3) a").classList.add("active");
+  }
+  else if (window.pageYOffset >= 1920 && window.pageYOffset < 2450) {
+    menu.querySelectorAll("a").forEach(element => element.classList.remove("active"));
+    menu.querySelector("li:nth-child(4) a").classList.add("active");
+  }
+  else if (window.pageYOffset >= 2450) {
+    menu.querySelectorAll("a").forEach(element => element.classList.remove("active"));
+    menu.querySelector("li:nth-child(5) a").classList.add("active");
+  };
+});
+
+menu.addEventListener("click", (event) => {
+  menu.querySelectorAll("a").forEach(element => element.classList.remove("active"));
+  event.target.classList.add("active");
+});
+
+ibutton_ver.addEventListener("click", (event) => {
+  let iphone = document.querySelector(".iphone-vertical img");
+  if (iphone.getAttribute("src") == "assets/images/iphone-vertical.png") {
+    iphone.setAttribute("src", "assets/images/black-screen-vertical.png");
+  }
+  else {
+    iphone.setAttribute("src", "assets/images/iphone-vertical.png");
+  };
+});
+
+ibutton_hor.addEventListener("click", (event) => {
+  let iphone = document.querySelector(".iphone-horizontal img");
+  if (iphone.getAttribute("src") == "assets/images/iphone-horizontal.png") {
+    iphone.setAttribute("src", "assets/images/black-screen-horizontal.png");
+  }
+  else {
+    iphone.setAttribute("src", "assets/images/iphone-horizontal.png");
+  };
 });
